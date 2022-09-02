8c3772bd3f335fbc8aed45c90312c027c0909a44
abdulla.ashurov <abdulla.ashurov@dsr-corporation.com>
2022-09-02 19:53:41 +0500

    Refactor code

--- include/header.hpp
+++ include/header.hpp
@@ -52,18 +52,18 @@ size_t calculate_size_of_packages_in_bytes(
     return (package_header_size_in_bytes + length_of_sender_name) * count_of_packages + length_of_sender_message;
 }
 
-void setFlag(std::vector<uint8_t>::iterator it) {
+void set_flag(std::vector<uint8_t>::iterator it) {
     const uint8_t defaultFlag = 0x5;
     *it = defaultFlag;
     *it <<= 5;
 }
 
-void setNameLength(std::vector<uint8_t>::iterator it, const uint8_t length) {
+void set_name_length(std::vector<uint8_t>::iterator it, const uint8_t length) {
     uint8_t temp = length << 1;
     *it |= temp;
 }
 
-void setMsgLength(std::vector<uint8_t>::iterator it, const uint8_t length) {
+void set_msg_length(std::vector<uint8_t>::iterator it, const uint8_t length) {
     uint8_t value_to_first_byte = (length & 0x10) >> 4;
     *it |= value_to_first_byte;
 
@@ -71,23 +71,23 @@ void setMsgLength(std::vector<uint8_t>::iterator it, const uint8_t length) {
     *(it + 1) = value_to_second_byte;
 }
 
-void setCRC4(std::vector<uint8_t>::iterator it, const uint8_t value) {
+void set_crc4(std::vector<uint8_t>::iterator it, const uint8_t value) {
     *it |= value;
 }
 
-void setHeader(std::vector<uint8_t>::iterator it, const uint8_t name_length, const uint8_t msg_length, const uint8_t crc4) {
-    setFlag(it);
-    setNameLength(it, name_length);
-    setMsgLength(it, msg_length);
-    setCRC4(it + 1, crc4);
+void set_header(std::vector<uint8_t>::iterator it, const uint8_t name_length, const uint8_t msg_length, const uint8_t crc4) {
+    set_flag(it);
+    set_name_length(it, name_length);
+    set_msg_length(it, msg_length);
+    set_crc4(it + 1, crc4);
 }
 
-void setSenderName(std::vector<uint8_t>::iterator begin, std::vector<uint8_t>::iterator end, const std::string &name) {
+void set_sender_name(std::vector<uint8_t>::iterator begin, std::vector<uint8_t>::iterator end, const std::string &name) {
     for(int i = 0; begin != end; i++, begin++) {
         *begin = name[i];
     }
 }
-void setSenderMsg(std::vector<uint8_t>::iterator begin, std::vector<uint8_t>::iterator end, const std::string &msg, uint8_t start_index) {
+void set_sender_msg(std::vector<uint8_t>::iterator begin, std::vector<uint8_t>::iterator end, const std::string &msg, uint8_t start_index) {
     for(; begin != end; start_index++, begin++) {
         *begin = msg[start_index];
     }
@@ -114,20 +114,20 @@ std::vector<uint8_t> make_buff(const Msg_t &msg) {
     auto it = bytes_of_packages.begin();
     for (size_t i = 0, msg_length = msg.text.length(), start_msg_index = 0; i < count_of_packages || it != bytes_of_packages.end(); i++) {
         if (msg_length > max_length_of_message_in_one_package)
-            setHeader(it, msg.name.length(), max_length_of_message_in_one_package, crc4(4, 4, 4));
+            set_header(it, msg.name.length(), max_length_of_message_in_one_package, crc4(4, 4, 4));
         else 
-            setHeader(it, msg.name.length(), msg_length, crc4(4, 4, 4));
+            set_header(it, msg.name.length(), msg_length, crc4(4, 4, 4));
         it += package_header_size_in_bytes;
         
-        setSenderName(it, it + msg.name.length(), msg.name);
+        set_sender_name(it, it + msg.name.length(), msg.name);
         it += msg.name.length();
 
         if (msg_length > max_length_of_message_in_one_package) {
-            setSenderMsg(it, it + max_length_of_message_in_one_package, msg.text, start_msg_index);
+            set_sender_msg(it, it + max_length_of_message_in_one_package, msg.text, start_msg_index);
             it += max_length_of_message_in_one_package;
             start_msg_index += max_length_of_message_in_one_package;
         } else {
-            setSenderMsg(it, it + msg_length, msg.text, start_msg_index);
+            set_sender_msg(it, it + msg_length, msg.text, start_msg_index);
             it += msg_length;
         }
     }
