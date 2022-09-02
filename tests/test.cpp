1e5a58d33b3a7e5d78462aecd5dc2ee0a19b8551
abdulla.ashurov <abdulla.ashurov@dsr-corporation.com>
2022-09-02 19:58:44 +0500

    Add unit test for functions: set_flag and set_name_length

--- tests/test.cpp
+++ tests/test.cpp
@@ -109,28 +109,65 @@ TEST_CASE("test function calculate_size_of_packages_in_bytes") {
     }
 }
 
-TEST_CASE("test function setHeader") {
-    SECTION("In vector shoudld be {169, 111} values") {
+TEST_CASE("test function set_flag") {
+    SECTION("in vector should be {160} value") {
+        std::vector<uint8_t> package(1);
+        set_flag(package.begin());
+
+        const size_t expected_value = 160;
+        REQUIRE(package[0] == expected_value);
+    }
+}
+
+TEST_CASE("test function set_name_length") {
+    SECTION("in vector should be {0} value") {
+        std::vector<uint8_t> package(1);
+        const size_t name_length = 0;
+        set_name_length(package.begin(), name_length);
+
+        const size_t expected_value = 0;
+        REQUIRE(package[0] == expected_value);
+    }
+    SECTION("in vector should be {28} value") {
+        std::vector<uint8_t> package(1);
+        const size_t name_length = 7;
+        set_name_length(package.begin(), name_length);
+
+        const size_t expected_value = 14;
+        REQUIRE(package[0] == expected_value);
+    }
+    SECTION("in vector should be {30} value") {
+        std::vector<uint8_t> package(1);
+        const size_t name_length = 15;
+        set_name_length(package.begin(), name_length);
+
+        const size_t expected_value = 30;
+        REQUIRE(package[0] == expected_value);
+    }
+}
+
+TEST_CASE("test function set_header") {
+    SECTION("in vector should be {169, 111} values") {
         std::vector<uint8_t> package(2);
         const size_t length_of_sender_name = 4;
         const size_t length_of_sender_message = 22;
         const size_t crc4_value = 15;
-        setHeader(package.begin(), length_of_sender_name, length_of_sender_message, crc4_value);
+        set_header(package.begin(), length_of_sender_name, length_of_sender_message, crc4_value);
 
         std::vector<uint8_t> expected_values{169, 111};
         
         REQUIRE(package[0] == expected_values[0]);
         REQUIRE(package[1] == expected_values[1]);  
     }
-    SECTION("In vector shoudld be {169, 255} values") {
+    SECTION("in vector should be {169, 255} values") {
         std::vector<uint8_t> package(2);
         const size_t length_of_sender_name = 4;
         const size_t length_of_sender_message = 31;
         const size_t crc4_value = 15;
-        setHeader(package.begin(), length_of_sender_name, length_of_sender_message, crc4_value);
+        set_header(package.begin(), length_of_sender_name, length_of_sender_message, crc4_value);
 
         std::vector<uint8_t> expected_values{169, 255};
-        
+
         REQUIRE(package[0] == expected_values[0]);
         REQUIRE(package[1] == expected_values[1]);  
     }
