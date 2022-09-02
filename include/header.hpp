ffcfe3205ebcfc6bae2823c19227dac993039c94
abdulla.ashurov <abdulla.ashurov@dsr-corporation.com>
2022-09-02 16:02:01 +0500

    Refactor code

--- include/header.hpp
+++ include/header.hpp
@@ -2,6 +2,9 @@
 #define HEADER
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include <stdexcept>
 
 struct Msg_t {
     std::string name;
@@ -10,38 +13,123 @@ struct Msg_t {
     Msg_t(const std::string &name, const std::string &text): name(name), text(text) {}
 };
 
-void setFlag();
-void setNameLength();
-void setMsgLength();
-void setCRC4();
-void setSenderName();
-void setSenderMsg();
+static const uint8_t crc4_tab[] = {
+	0x0, 0x7, 0xe, 0x9, 0xb, 0xc, 0x5, 0x2,
+	0x1, 0x6, 0xf, 0x8, 0xa, 0xd, 0x4, 0x3,
+};
+/**
+ * crc4 - calculate the 4-bit crc of a value.
+ * @c:    starting crc4
+ * @x:    value to checksum
+ * @bits: number of bits in @x to checksum
+ *
+ * Returns the crc4 value of @x, using polynomial 0b10111.
+ *
+ * The @x value is treated as left-aligned, and bits above @bits are ignored
+ * in the crc calculations.
+ */
+uint8_t crc4(uint8_t c, uint64_t x, int bits)
+{
+	int i;
+	/* mask off anything above the top bit */
+	x &= (1ull << bits) - 1;
+	/* Align to 4-bits */
+	bits = (bits + 3) & ~0x3;
+	/* Calculate crc4 over four-bit nibbles, starting at the MSbit */
+	for (i = bits - 4; i >= 0; i -= 4)
+		c = crc4_tab[c ^ ((x >> i) & 0xf)];
+	
+    return c;
+}
 
-const size_t calculate_count_of_packages(const size_t length_of_sender_message, const size_t max_length_of_message_in_one_package) {
+size_t calculate_count_of_packages(const size_t length_of_sender_message, const size_t max_length_of_message_in_one_package) {
     return (length_of_sender_message / max_length_of_message_in_one_package) + ((length_of_sender_message % max_length_of_message_in_one_package) ? 1 : 0);
 }
 
-const size_t calculate_size_of_packages_in_bytes(
+size_t calculate_size_of_packages_in_bytes(
     const size_t package_header_size_in_bytes, const size_t length_of_sender_name, const size_t length_of_sender_message, const size_t count_of_packages
 ) {
     return (package_header_size_in_bytes + length_of_sender_name) * count_of_packages + length_of_sender_message;
 }
 
-// std::vector<uint8_t> make_buff(const Msg_t &msg) {
-//     const size_t max_length_of_sender_name = 15;
-//     if (msg.name.empty() || msg.name.length() > max_length_of_sender_name) {
-//         throw std::length_error("The sender's name must be between 1 and 15 characters.");
-//     }
+void setFlag(std::vector<uint8_t>::iterator it) {
+    const uint8_t defaultFlag = 0x5;
+    *it = defaultFlag;
+    *it <<= 5;
+}
 
-//     if (msg.text.empty()) {
-//         throw std::length_error("The sender's text message must not be empty.");
-//     }
+void setNameLength(std::vector<uint8_t>::iterator it, const uint8_t length) {
+    uint8_t temp = length << 1;
+    *it |= temp;
+}
 
-//     const size_t max_length_of_message_in_one_package = 31;
-//     const size_t count_of_packages = calculate_count_of_packages(msg.text.length(), max_length_of_message_in_one_package);
+void setMsgLength(std::vector<uint8_t>::iterator it, const uint8_t length) {
+    uint8_t value_to_first_byte = (length & 0x10) >> 4;
+    *it |= value_to_first_byte;
 
-//     const size_t package_header_size_in_bytes = 2;
-//     const size_t size = calculate_size_of_packages_in_bytes(package_header_size_in_bytes, msg.name.length(), msg.text.length(), count_of_packages);
-// }
+    uint8_t value_to_second_byte = length << 4;
+    *(it + 1) = value_to_second_byte;
+}
 
-#endif 
+void setCRC4(std::vector<uint8_t>::iterator it, const uint8_t value) {
+    *it |= value; 
+}
+
+void setHeader(std::vector<uint8_t>::iterator it, const uint8_t name_length, const uint8_t msg_length, const uint8_t crc4) {
+    setFlag(it);
+    setNameLength(it, name_length);
+    setMsgLength(it, msg_length);
+    setCRC4(it + 1, crc4);
+}
+
+void setSenderName(std::vector<uint8_t>::iterator begin, std::vector<uint8_t>::iterator end, const std::string &name) {
+    for(int i = 0; begin != end; i++, begin++) {
+        *begin = name[i];
+    }
+}
+void setSenderMsg(std::vector<uint8_t>::iterator begin, std::vector<uint8_t>::iterator end, const std::string &name, const uint8_t start_index) {
+    for(; begin != end; start_index++, begin++) {
+        *begin = msg[start_index];
+    }
+}
+
+std::vector<uint8_t> make_buff(const Msg_t &msg) {
+    const size_t max_length_of_sender_name = 15;
+    if (msg.name.empty() || msg.name.length() > max_length_of_sender_name) {
+        throw std::length_error("The sender's name must be between 1 and 15 characters.");
+    }
+
+    if (msg.text.empty()) {
+        throw std::length_error("The sender's text message must not be empty.");
+    }
+
+    const size_t max_length_of_message_in_one_package = 31;
+    const size_t count_of_packages = calculate_count_of_packages(msg.text.length(), max_length_of_message_in_one_package);
+
+    const size_t package_header_size_in_bytes = 2;
+    const size_t size = calculate_size_of_packages_in_bytes(package_header_size_in_bytes, msg.name.length(), msg.text.length(), count_of_packages);
+
+    std::vector<uint8_t> bytes_of_packages(size);
+
+    auto it = bytes_of_packages.begin();
+    for (size_t i = 0, msg_length = msg.text.length(), start_msg_index = 0; i < count_of_packages || it != bytes_of_packages.end(); i++) {
+        setHeader(it, msg.name.length(), msg.text.length(), crc4(4, 4, 4));
+        it += package_header_size_in_bytes;
+        
+        setSenderName(it, it + msg.name.length(), msg.name);
+        it += msg.name.length();
+
+        if ((i + 1) == size) {
+            setSenderMsg(it, it + msg_length, msg.text, start_msg_index);
+            it += msg_length;
+        } else {
+            setSenderMsg(it, it + max_length_of_message_in_one_package, msg.text, start_msg_index);
+            it += max_length_of_message_in_one_package;
+            start_msg_index += max_length_of_message_in_one_package;
+        }
+    }
+
+    return bytes_of_packages;
+}
+
+#endif
