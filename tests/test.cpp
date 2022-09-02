fec044b532e4299cb3f949900abb30e057efa90c
abdulla.ashurov <abdulla.ashurov@dsr-corporation.com>
2022-09-02 14:24:17 +0500

    Add unit tests for checking function calculate_size_of_packages_in_bytes

--- tests/test.cpp
+++ tests/test.cpp
@@ -66,3 +66,45 @@ TEST_CASE("check function calculate_count_of_packages") {
         REQUIRE(calculate_count_of_packages(length_of_sender_message, max_length_of_message_in_one_package) == expected_value);
     }
 }
+
+TEST_CASE("check function calculate_size_of_packages_in_bytes") {
+    SECTION("should be 23 bytes") {
+        const size_t package_header_size_in_bytes = 2;
+        const size_t length_of_sender_name = 4;
+        const size_t length_of_sender_message = 17;
+
+        const size_t max_length_of_message_in_one_package = 31;
+        const size_t count_of_packages = calculate_count_of_packages(length_of_sender_message, max_length_of_message_in_one_package);
+
+        const size_t expected_value = 23;
+        REQUIRE(calculate_size_of_packages_in_bytes(
+            package_header_size_in_bytes, length_of_sender_name, length_of_sender_message, count_of_packages) == expected_value
+        );
+    }
+    SECTION("should be 57 bytes") {
+        const size_t package_header_size_in_bytes = 2;
+        const size_t length_of_sender_name = 8;
+        const size_t length_of_sender_message = 37;
+
+        const size_t max_length_of_message_in_one_package = 31;
+        const size_t count_of_packages = calculate_count_of_packages(length_of_sender_message, max_length_of_message_in_one_package);
+
+        const size_t expected_value = 57;
+        REQUIRE(calculate_size_of_packages_in_bytes(
+            package_header_size_in_bytes, length_of_sender_name, length_of_sender_message, count_of_packages) == expected_value
+        );
+    }
+    SECTION("should be 144 bytes") {
+        const size_t package_header_size_in_bytes = 2;
+        const size_t length_of_sender_name = 15;
+        const size_t length_of_sender_message = 93;
+
+        const size_t max_length_of_message_in_one_package = 31;
+        const size_t count_of_packages = calculate_count_of_packages(length_of_sender_message, max_length_of_message_in_one_package);
+
+        const size_t expected_value = 144;
+        REQUIRE(calculate_size_of_packages_in_bytes(
+            package_header_size_in_bytes, length_of_sender_name, length_of_sender_message, count_of_packages) == expected_value
+        );
+    }
+}
