fc5fbae995ec4f1a45889f06c933c99b319be60e
abdulla.ashurov <abdulla.ashurov@dsr-corporation.com>
2022-09-09 16:31:49 +0500

    Remove incorrect file name

--- include/common.hpp
+++
@@ -1,6 +0,0 @@
-#include <string>
-#include <vector>
-
-const std::string::const_iterator push_single_packet(
-    const std::string &sender_name, const std::string &sender_message, const std::string::const_iterator end, const std::vector<uint8_t> buffer
-);
--- src/common.hpp
+++
@@ -1,7 +0,0 @@
-#include "../include/common.hpp"
-
-const std::string::const_iterator push_single_packet(
-    const std::string &sender_name, const std::string &sender_message, const std::string::const_iterator end, const std::vector<uint8_t> buffer
-){
-    
-}
